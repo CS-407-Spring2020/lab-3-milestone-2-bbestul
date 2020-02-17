526b497fe4842927dca259e8f4ef18558e77c96a
Blaine <blaine@wiscoradio.com>
2020-02-17 00:38:16 -0600

    Created Fragments

--- app/src/main/java/com/example/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab3milestone2/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.lab3milestone2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
+        import android.app.Fragment;
+        import android.app.FragmentManager;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.app.FragmentTransaction;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button firstFragement, secondFragement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get Reference of Buttons
+        firstFragement = findViewById(R.id.firstFragment);
+        secondFragement = findViewById(R.id.secondFragment);
+
+        // perform setOnClickListener Event for First button
+        firstFragement.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragement(new FirstFragment());
+            }
+        }));
+
+        secondFragement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragement(new SecondFragment());
+            }
+        });
+
+    }
+
+    private void loadFragement(Fragment fragment){
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
